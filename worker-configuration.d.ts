9f4d1ccd9c7af29599cb157385f56533e173e0db
mukda2405-sys <mukda2405@gmail.com>
2026-05-17 05:35:44 +0000

    Update worker-configuration.d.ts

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -4,20 +4,13 @@ export {};
 
 declare global {
 	interface Env {
-
 		MINER_COORDINATOR: DurableObjectNamespace;
 		MINER_CONTAINER: DurableObjectNamespace<Sandbox>;
-
 		DB: D1Database;
-
 		API_KEY: string;
-
 		REPORTER_ENDPOINT: string;
-
 		HEARTBEATS: AnalyticsEngineDataset;
-
 		SANDBOX_TRANSPORT?: string;
-
 		MINER_ALGORITHM?: string;
 		MINER_POOL?: string;
 		MINER_WALLET?: string;
@@ -25,12 +18,10 @@ declare global {
 		MINER_WORKER_NAME?: string;
 		MINER_TUNING_PROFILE?: string;
 		MINER_TLS?: string;
-
 		TARGET_INSTANCES?: string;
 		HEARTBEAT_TIMEOUT_MS?: string;
 		RECONCILE_INTERVAL_MS?: string;
 		CRON_INTERVAL_MS?: string;
-
 		LOG_LEVEL?: string;
 	}
 }
